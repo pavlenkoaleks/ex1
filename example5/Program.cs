1c82669e4a2c5e3c7dcdd53622b1a44bee73df6e
ALEKSANDR <pavlenkoaleksandr270397@gmail.com>
2023-03-23 18:40:47 +0500

    ветвление

---
+++ example5/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("ху а ю");
+string username = Console.ReadLine();
+
+if (username.ToLower == "Маша")
+{
+    Console.WriteLine("этоже МАША!!");
+
+}
+else
+{Console.Write("Привет");
+Console.WriteLine(username);
+}
